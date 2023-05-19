be9494b0912d1d2cddecff7eee4cca9172b6b0aa
dstilesr <d.stiles.r@hotmail.com>
2023-05-19 17:18:10 -0500

    Improve logging on subscriber

--- subscriber/api/handlers.py
+++ subscriber/api/handlers.py
@@ -2,19 +2,21 @@ import logging
 from config import SUBSCRIBER
 
 
-class SubscribeHandler(object):
+class SubscribeHandler:
 
-    @classmethod
-    def process(cls, **kwargs):
+    def __init__(self):
+        self.logger = logging.getLogger("subscriber")
+        self.logger.setLevel(logging.INFO)
+
+    def process(self, **kwargs):
         """
         Processes a message received from SNS.
-
         :param kwargs: Request arguments
         :return:
         """
         try:
-            logging.info(f"MESSAGE RECEIVED. TYPE: {kwargs['Type']}")
-            print(kwargs["Message"])
+            self.logger.info(f"MESSAGE RECEIVED. TYPE: {kwargs['Type']}")
+            self.logger.info(kwargs["Message"])
 
             if kwargs['Type'] == "Notification":
                 logging.info("NOTIFICATION_RECEIVED")
@@ -26,5 +28,5 @@ class SubscribeHandler(object):
             return True
 
         except Exception as e:
-            print(e)
+            self.logger.error("Subscriber fail! %s" % str(e))
             return False
